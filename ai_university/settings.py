948cf695829fe60d609f6a1a885c24b526bf7ebb
medelz-awais <mawais@medelz.com>
2026-09-05 12:49:55 +0500

    Configure dj-database-url for PostgreSQL persistence

--- ai_university/settings.py
+++ ai_university/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,10 +57,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ai_university.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}"),
+        conn_max_age=600
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.User'
